ff2b22ea97a4fa4f087a099fd78acae36b53c9b4
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-05 17:52:25 -0700

    ReverseString Options 3, 4, and 5 completed.

--- ReverseString/JSSolution.js
+++ ReverseString/JSSolution.js
@@ -25,8 +25,31 @@ reverseString1c = string => {
     return reversed;
 }
 
-// Option 2:
+// Option 2: split(), reverse(), and join()
+reverseString2 = string => {
+    return string.split("").reverse().join("");
+}
 
+// Option 3: Spread Operator
+reverseString3 = string => {
+    return [...string].reverse().join("");
+}
+
+// Option 4: reduce()
+reverseString4 = string => {
+    return string.split("").reduce((rev, char)=> char + rev, ''); 
+}
+
+// Option 5: Recursion
+reverseString5 = string => {
+    if (string === "") {
+        return string;
+    } else {
+        return reverseString5(string.substr(1)) + string[0];    
+    }
+}
+
+// ****************
 // Tests:
 let test1aResult = reverseString1a('hello');
 console.log('Option 1a:', test1aResult);
@@ -36,3 +59,15 @@ console.log('Option 1b:', test1bResult);
 
 let test1cResult = reverseString1c('hello');
 console.log('Option 1c:', test1cResult);
+
+let test2Result = reverseString2('hello');
+console.log('Option 2:', test2Result);
+
+let test3Result = reverseString3('hello');
+console.log('Option 3:', test3Result);
+
+let test4Result = reverseString4('hello');
+console.log('Option 4:', test4Result);
+
+let test5Result = reverseString5('hello');
+console.log('Option 5:', test5Result);
