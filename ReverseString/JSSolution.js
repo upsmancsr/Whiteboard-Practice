49ae182c002e4f2d430e03574a62962f2ee0ee0b
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-05 17:15:36 -0700

    ReverseString JS options 1a and 1b complete.

--- ReverseString/JSSolution.js
+++ ReverseString/JSSolution.js
@@ -1,5 +1,5 @@
-// Option 1: for loop
-function reverseString1(string) {
+// Option 1a: for loop
+reverseString1a = string => {
     let reversed = "";
     for (let i = string.length - 1; i >= 0; i--) {
         reversed += string[i]; 
@@ -7,5 +7,32 @@ function reverseString1(string) {
     return reversed;
 }
 
-let test1Result = reverseString1('hello');
-console.log(test1Result);
+// Option 1b:
+reverseString1b = string => {
+    let reversed = "";
+    for (let i = 0; i < string.length; i++) {
+        reversed = string[i] + reversed; 
+    }
+    return reversed;
+}
+
+// Option 1c:
+reverseString1c = string => {
+    let reversed = "";
+    for (let char of string) {
+        reversed = char + reversed; 
+    }
+    return reversed;
+}
+
+// Option 2:
+
+// Tests:
+let test1aResult = reverseString1a('hello');
+console.log('Option 1a:', test1aResult);
+
+let test1bResult = reverseString1b('hello');
+console.log('Option 1b:', test1bResult);
+
+let test1cResult = reverseString1c('hello');
+console.log('Option 1c:', test1cResult);
