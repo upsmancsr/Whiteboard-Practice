f26159ec8c0b52757adf7f14176cb4d1673be821
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-07-09 13:43:53 -0700

    Completed another alternative recursive fib solution.

--- NthFibonacci/JSSolution.js
+++ NthFibonacci/JSSolution.js
@@ -22,26 +22,51 @@
 
 // Solution using recursion and memoization:
 
+// function fib(n) {
+//     let cache = {};
+
+//     function f(n) {
+//         let value = null;
+
+//         if (n in cache) {
+//             value = cache[n];
+//         } else {    
+//             if (n < 2) {
+//                 value = n;
+//             } else {
+//                 value = f(n-2) + f(n-1);  // triggers recursive calls to f()
+//             }
+//             cache[n] = value;
+//         }
+//         return value;
+//     }
+//     return f(n);
+// }
+
+// Solution using recursion and memo, with separate function for calculating fib
+
 function fib(n) {
-    let cache = {};
+    let cache = Array(n);
 
-    function f(n) {
-        let value = null;
-
-        if (n in cache) {
-            value = cache[n];
-        } else {    
-            if (n < 2) {
-                value = n;
-            } else {
-                value = f(n-2) + f(n-1);
-            }
+    function fibMemo(n) {
+        let value = cache[n];
+        // if there is no value at cache[n]:
+        if (!value) {
+            value = fibRecurse(n);
             cache[n] = value;
         }
         return value;
     }
-    return f(n);
+
+    function fibRecurse(n) {
+        if (n < 2) {
+            return n;
+        } // else...
+        return fibMemo(n - 2) + fibMemo(n - 1);
+    }
+
+    return fibMemo(n);
 }
 
-
+console.log('fib file run');
 console.log(fib(11));
