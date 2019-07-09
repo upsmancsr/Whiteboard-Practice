0cdaf9d23bc5ed74e2fa124d38f18c752f2a7cad
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-07-09 15:07:47 -0700

    memoizedAddTen function is written but not utilizing cache

--- Memoize/JSExamples.js
+++ Memoize/JSExamples.js
@@ -3,4 +3,24 @@ function addTen(n) {
     return sum = n + 10;
 }
 
-console.log(addTen(9));
+// memoizedAddTen function specific to addTen:
+function memoizedAddTen(n) {
+    let cache = {};
+
+    function f(n) {
+        if (n in cache) {
+            console.log('result found in cache')
+            return cache[n];
+        } else {
+            console.log('calculating result');
+            let result = n + 10;
+            cache[n] = result;
+            return result;
+        }
+    }
+    return f(n);
+}
+
+//const newAdd = memoizedAddTen();
+console.log(memoizedAddTen(9));
+console.log(memoizedAddTen(9));
